ea9b309ccc7ecd4da1e50dd0096c923531bf595b
yuxiang.chen <yuxiang.chen@qunar.com>
2017-08-03 10:32:49 +0800

    工程初始化

---
+++ src/main/java/cn/agentd/code/App.java
@@ -0,0 +1,12 @@
+package cn.agentd.code;
+
+/**
+ * Created by yuxiang.chen on 2017/8/3.
+ *
+ * 初始化
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Codelet");
+    }
+}
